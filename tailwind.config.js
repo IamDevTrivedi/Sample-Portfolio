6b2242c5f773e685eb2e989b2e69cca92d945dc4
IamDevTrivedi <devtrivedi22082008@gmail.com>
2024-07-27 19:06:57 +0530

    files Uploaded

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          darkTheme: '#11001F',
+        },
+      },
+    },
+
+    darkMode: 'selector' ,
+    variants: {},
+    plugins: [],
+  };
+  
